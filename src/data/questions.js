f13990a342eef66afacdd9152e150d3358803c2b
JIYEON_OnTime <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-10-22 17:27:14 +0900

    Add files via upload

--- src/data/questions.js
+++ src/data/questions.js
@@ -2,109 +2,248 @@ export const questions = [
     {
         Q: "제트썬 앞에서 제대로 해야하는 것은?",
         image: "",
-        correct:"윌리바운스",
-        incorrect:"트월킹",
-        score:3,
+        A1:{
+            A:"트월킹",
+            score:0,
+        },
+        A2:{
+            A:"윌리바운스",
+            score:3,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
         Q: "가비가 하는 장사는?",
         image: "",
-        correct:"속눈썹",
-        incorrect:"가발",
-        score:3,
+        A1:{
+            A:"가발",
+            score:0,
+        },
+        A2:{
+            A:"속눈썹",
+            score:3,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
         Q: "이 장면의 대사를 맞춰주세요!",
         image: "",
-        correct:"대잔치처럼 너무 재미있을 거 같아요",
-        incorrect:"믓찌다(?) 믓찌다(?) 울언니",
-        score:6,
+        A1:{
+            A:"대잔치처럼 너무 재미있을 거 같아요",
+            score:6,
+        },
+        A2:{
+            A:"믓찌다(?) 믓찌다(?) 울언니",
+            score:0,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
         Q: "솔직히 헤이마마 춤춰본적 있다? vs 없다?",
         image: "",
-        correct:"있다",
-        incorrect:"없다",
-        score:6,
+        A1:{
+            A:"⭕️ 있다",
+            score:6,
+        },
+        A2:{
+            A:"❌ 없다",
+            score:0,
+        },
+        A3:{
+            A:'',
+            score:0,
+        },
     },
     {
-        Q: "본명이 혜인인 멤버가 없는 크루는?",
+        Q: "본명이 혜인인 멤버가 ",
+        Q1:"없는 ",
+        Q2: "크루는?",
         image: "",
-        correct:"윈트",
-        incorrect:"코카앤버터",
-        score:6,
+        A1:{
+            A:"코카앤버터",
+            score:0,
+        },
+        A2:{
+            A:"원트",
+            score:6,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
-        Q: "ygx가 외친 구호가 아닌 것은?",
+        Q: "YGX가 외친 구호가 ",
+        Q1: "아닌 ",
+        Q2: "것은?",
         image: "",
-        correct:"찢자 찢자 Fight!",
-        incorrect:"괜찮아 괜찮아 Aghit!",
-        score:6,
+        A1:{
+            A:"찢자 찢자 Fight!",
+            score:6,
+        },
+        A2:{
+            A:"괜찮아 괜찮아 Aghit!",
+            score:0,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     
     {
         Q: "리헤이가 입을 가린 이유는?",
         image: "",
-        correct:"김밥을 먹고 있어서",
-        incorrect:"비트박스를 하기 위해",
-        score:10
+        A1:{
+            A:"김밥을 먹고 있어서",
+            score:10,
+        },
+        A2:{
+            A:"비트박스를 하기 위해",
+            score:0,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
         Q: "홀리뱅 제인의 별명이 된 것은?",
         image: "",
-        correct:"나무",
-        incorrect:"잡초",
-        score:6
+        A1:{
+            A:"나무",
+            score:6,
+        },
+        A2:{
+            A:"잡초",
+            score:0,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
         Q: "스우파 출연 댄서 인스타 5명 이상 팔로우 했다",
         image: "",
-        correct:"했다",
-        incorrect:"안했다",
-        score:6
+        A1:{
+            A:"⭕️ 했다",
+            score:6,
+        },
+        A2:{
+            A:"❌ 안했다",
+            score:0,
+        },
+        A3:{
+            A:'',
+            score:0,
+        },
     },
     {
         Q: "빠른 92인 효진초이는 92년생과 친구를 할 수 있다? vs 없다?",
         image: "",
-        correct:"족보 꼬여서 안됨",
-        incorrect:"92년생과 칭구칭긔",
-        score:10
+        A1:{
+            A:"❌ 족보 꼬여서 안됨",
+            score:10,
+        },
+        A2:{
+            A:"⭕️ 92년생과 칭구칭긔",
+            score:0,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
         Q: "이 장면의 대사를 맞춰주세요!",
         image: "",
-        correct:"케.라.라.케",
-        incorrect:"케.리.리.케",
-        score:6
+        A1:{
+            A:"케.리.리.케",
+            score:0,
+        },
+        A2:{
+            A:"케.라.라.케",
+            score:6,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
         Q: "리정이 먹고 입에서 녹아내린다고 한 음식은?",
         image: "",
-        correct:"성게알",
-        incorrect:"참치회",
-        score:10
+        A1:{
+            A:"성게알",
+            score:10,
+        },
+        A2:{
+            A:"참치회",
+            score:0,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
         Q: "윤경이가 몇학년때 아이키를 만났을까?",
         image: "",
-        correct:"6학년",
-        incorrect:"5학년",
-        score:6
+        A1:{
+            A:"5학년",
+            score:0,
+        },
+        A2:{
+            A:"6학년",
+            score:6,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
 
     {
         Q: "노제의 MBTI는?",
         image: "",
-        correct:"ENFP",
-        incorrect:"ISFP",
-        score:10,
+        A1:{
+            A:"ENTP",
+            score:0,
+        },
+        A2:{
+            A:"ISFP",
+            score:10,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
     {
         Q: "현재 같은 집에서 함께 사는 댄서는?",
         image: "",
-        correct:"모니카-립제이",
-        incorrect:"리헤이-제트썬",
-        score:6,
+        A1:{
+            A:"모니카-립제이",
+            score:6,
+        },
+        A2:{
+            A:"리헤이-제트썬",
+            score:0,
+        },
+        A3:{
+            A:'모름',
+            score:0,
+        },
     },
 
 ]
