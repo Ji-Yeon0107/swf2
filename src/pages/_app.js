c40cc16938084ad5a88373b935441c56cc7e640e
JIYEON_OnTime <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-10-22 19:48:35 +0900

    Add files via upload

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,7 +19,7 @@ const App = ({ Component }) => (
             <meta property="og:url" content="https://swf-alpha.vercel.app/" />
             <meta property="og:title" content="스우파 과몰입꾼 테스트" />
             <meta property="og:description" content="헤이마마좀 춰본 정도..?" />
-            <meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
+            <meta property="og:image" content="https://raw.githubusercontent.com/Ji-Yeon0107/swf2/main/public/logo.png" />
             <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
             <script>
                 Kakao.init('178a2bb850640c5dee765d4a93e39bfb');
