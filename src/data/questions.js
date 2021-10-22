5bbdfa461af67c32aed41a1eea00fb54ee1b3938
JIYEON_OnTime <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-10-22 13:30:59 +0900

    Add files via upload

--- src/data/questions.js
+++ src/data/questions.js
@@ -2,24 +2,46 @@ export const questions = [
     {
         Q: "제트썬 앞에서 제대로 해야하는 것은?",
         image: "",
-        correct:"트월킹",
-        incorrect:"윌리바운스",
+        correct:"윌리바운스",
+        incorrect:"트월킹",
         score:3,
     },
     {
         Q: "가비가 하는 장사는?",
         image: "",
-        correct:"가발",
-        incorrect:"속눈썹",
-        score:6,
+        correct:"속눈썹",
+        incorrect:"가발",
+        score:3,
     },
     {
-        Q: "대사를 맞춰주세요!",
+        Q: "이 장면의 대사를 맞춰주세요!",
         image: "",
         correct:"대잔치처럼 너무 재미있을 거 같아요",
         incorrect:"믓찌다(?) 믓찌다(?) 울언니",
         score:6,
     },
+    {
+        Q: "솔직히 헤이마마 춤춰본적 있다? vs 없다?",
+        image: "",
+        correct:"있다",
+        incorrect:"없다",
+        score:6,
+    },
+    {
+        Q: "본명이 혜인인 멤버가 없는 크루는?",
+        image: "",
+        correct:"윈트",
+        incorrect:"코카앤버터",
+        score:6,
+    },
+    {
+        Q: "ygx가 외친 구호가 아닌 것은?",
+        image: "",
+        correct:"찢자 찢자 Fight!",
+        incorrect:"괜찮아 괜찮아 Aghit!",
+        score:6,
+    },
+    
     {
         Q: "리헤이가 입을 가린 이유는?",
         image: "",
@@ -28,6 +50,49 @@ export const questions = [
         score:10
     },
     {
+        Q: "홀리뱅 제인의 별명이 된 것은?",
+        image: "",
+        correct:"나무",
+        incorrect:"잡초",
+        score:6
+    },
+    {
+        Q: "스우파 출연 댄서 인스타 5명 이상 팔로우 했다",
+        image: "",
+        correct:"했다",
+        incorrect:"안했다",
+        score:6
+    },
+    {
+        Q: "빠른 92인 효진초이는 92년생과 친구를 할 수 있다? vs 없다?",
+        image: "",
+        correct:"족보 꼬여서 안됨",
+        incorrect:"92년생과 칭구칭긔",
+        score:10
+    },
+    {
+        Q: "이 장면의 대사를 맞춰주세요!",
+        image: "",
+        correct:"케.라.라.케",
+        incorrect:"케.리.리.케",
+        score:6
+    },
+    {
+        Q: "리정이 먹고 입에서 녹아내린다고 한 음식은?",
+        image: "",
+        correct:"성게알",
+        incorrect:"참치회",
+        score:10
+    },
+    {
+        Q: "윤경이가 몇학년때 아이키를 만났을까?",
+        image: "",
+        correct:"6학년",
+        incorrect:"5학년",
+        score:6
+    },
+
+    {
         Q: "노제의 MBTI는?",
         image: "",
         correct:"ENFP",
@@ -35,73 +100,11 @@ export const questions = [
         score:10,
     },
     {
-        Q: "방송에 출연한 훅 크루 멤버 수는?",
+        Q: "현재 같은 집에서 함께 사는 댄서는?",
         image: "",
-        correct:"6명",
-        incorrect:"7명",
+        correct:"모니카-립제이",
+        incorrect:"리헤이-제트썬",
         score:6,
     },
-    {
-        Q: "본명이 혜인인 멤버가 없는 크루는?",
-        image: "",
-        correct:"훅",
-        incorrect:"YGX",
-        score:6,
-    },
-    {
-        Q: "나이가 더 어린 댄서는?",
-        image: "",
-        correct:"선윤경",
-        incorrect:"시미즈",
-        score:6,
-    },
-    {
-        Q: "글로벌 평가 미션에서 모자를 쓴 크루는?",
-        image: "",
-        correct:"원트",
-        incorrect:"프라우드먼",
-        score:10,
-    },
-    {
-        Q: "글로벌 평가 미션에서 모자를 쓴 크루는?",
-        image: "",
-        correct:"원트",
-        incorrect:"프라우드먼",
-        score:10,
-    },
-    {
-        Q: "글로벌 평가 미션에서 모자를 쓴 크루는?",
-        image: "",
-        correct:"원트",
-        incorrect:"프라우드먼",
-        score:10,
-    },
-    {
-        Q: "글로벌 평가 미션에서 모자를 쓴 크루는?",
-        image: "",
-        correct:"원트",
-        incorrect:"프라우드먼",
-        score:10,
-    },
-    {
-        Q: "글로벌 평가 미션에서 모자를 쓴 크루는?",
-        image: "",
-        correct:"원트",
-        incorrect:"프라우드먼",
-        score:10,
-    },
-    {
-        Q: "글로벌 평가 미션에서 모자를 쓴 크루는?",
-        image: "",
-        correct:"원트",
-        incorrect:"프라우드먼",
-        score:10,
-    },
-    {
-        Q: "글로벌 평가 미션에서 모자를 쓴 크루는?",
-        image: "",
-        correct:"원트",
-        incorrect:"프라우드먼",
-        score:10,
-    },
+
 ]
