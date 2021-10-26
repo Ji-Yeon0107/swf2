101071ef08912be585d0c67e2669ac2d46d5f6f7
JIYEON_OnTime <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-10-26 13:17:55 +0900

    Add files via upload

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -34,7 +34,7 @@ const App = ({ Component }) => {
         </script>
             {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2695315085641819" crossorigin="anonymous"></script> */}
             <meta name="naver-site-verification" content="dbca9d353538b4bbea1f9810bd4fc39c9165ab0b" />
-            <meta name="description" content="스우파 과몰입꾼 테스트 하고 가세요." />
+            <meta name="description" content="스우파 과몰입 테스트 하고 가세요." />
             <meta name="twitter:card" content="summary" />
             <meta property="og:url" content="https://swf-alpha.vercel.app/" />
             <meta property="og:title" content="스우파 과몰입꾼 테스트" />
